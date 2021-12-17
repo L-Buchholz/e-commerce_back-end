82e86ad65842db1f8177471e8efceb7ac8f9006d
Lauren Buchholz <90865074+L-Buchholz@users.noreply.github.com>
2021-12-16 17:36:48 -0700

    Final commit for now

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -83,50 +83,52 @@ will be for POST and PUT routes!
 */
 // Code imported from product-routes.js
 router.put("/:id", (req, res) => {
-  Tag.update(
-    // (Updated code)
-    {
-      tag_name: req.body.tag_name,
-    },
-    {
-      where: {
-        id: req.params.id,
+  return (
+    Tag.update(
+      // (Updated code)
+      {
+        tag_name: req.body.tag_name,
       },
-    }
-    // (Updated code ends here)
-  )
-    // (Imported/modified product-routes code begins here)
-    .then((tag) => {
-      // Find all associated products from ProductTag
-      return ProductTag.findAll({ where: { tag_id: req.params.id } });
-    })
-    .then((productTags) => {
-      // Get list of current product_ids
-      const productTagIds = productTags.map(({ product_id }) => product_id);
-      // Create filtered list of new product_ids
-      const newProductTags = req.body.productIds
-        .filter((product_id) => !productTagIds.includes(product_id))
-        .map((product_id) => {
-          return {
-            tag_id: req.params.id,
-            product_id,
-          };
-        });
-      // Figure out which ones to remove
-      const productTagsToRemove = productTags
-        .filter(({ product_id }) => !req.body.productIds.includes(product_id))
-        .map(({ id }) => id);
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+      // (Updated code ends here)
+    )
+      // (Imported/modified product-routes code begins here)
+      .then((tag) => {
+        // Find all associated products from ProductTag
+        return ProductTag.findAll({ where: { tag_id: req.params.id } });
+      })
+      .then((productTags) => {
+        // Get list of current product_ids
+        const productTagIds = productTags.map(({ product_id }) => product_id);
+        // Create filtered list of new product_ids
+        const newProductTags = req.body.productIds
+          .filter((product_id) => !productTagIds.includes(product_id))
+          .map((product_id) => {
+            return {
+              tag_id: req.params.id,
+              product_id,
+            };
+          });
+        // Figure out which ones to remove
+        const productTagsToRemove = productTags
+          .filter(({ product_id }) => !req.body.productIds.includes(product_id))
+          .map(({ id }) => id);
 
-      // Run both actions
-      return Promise.all([
-        ProductTag.destroy({ where: { id: productTagsToRemove } }),
-        ProductTag.bulkCreate(newProductTags),
-      ]);
-    })
-    .then((updatedProductTags) => res.json(updatedProductTags))
-    .catch((err) => {
-      res.status(400).json(err);
-    });
+        // Run both actions
+        return Promise.all([
+          ProductTag.destroy({ where: { id: productTagsToRemove } }),
+          ProductTag.bulkCreate(newProductTags),
+        ]);
+      })
+      .then((updatedProductTags) => res.json(updatedProductTags))
+      .catch((err) => {
+        res.status(400).json(err);
+      })
+  );
   // (Imported/modified product-routes code ends here)
 });
 
